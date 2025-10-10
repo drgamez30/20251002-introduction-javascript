cb20dc46ca1e700fe12cbf6bd9cf41a41578f524
drgamez <dr.gamez30@ciaf.edu.co>
2025-10-09 19:51:21 -0500

    Agrego ejercicio 1

---
+++ exercises/01-ejercicio.js
@@ -0,0 +1,9 @@
+// Declara una variable nombre y otra hobby. Imprime un mensaje
+// como:
+// "Hola, soy Juan y me gusta el fútbol", pero usando
+// concatenación con + y luego usando template strings.
+
+
+let nombre = prompt("Ingresa tu nombre:");
+let hobby = prompt("Ingresa tu hobby:");
+console.log(`Hola, soy ${nombre} y me gusta ${hobby}.`);
