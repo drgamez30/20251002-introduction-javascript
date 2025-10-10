1993df24d652ab4c96c03c7e26dde9a9b7e16299
drgamez <dr.gamez30@ciaf.edu.co>
2025-10-09 20:18:13 -0500

    Añado ejercicio 9

---
+++ exercises/09-ejercicio.js
@@ -0,0 +1,8 @@
+// 9. Insertar en Posición Específica (splice())
+// Dado ["manzana","pera","uva"], inserta "naranja" en la segunda
+// posición sin reemplazar a los demás.
+
+let frutas = ["manzana", "pera", "uva"];
+
+frutas.splice(1, 0, "naranja");
+console.log(frutas); 
